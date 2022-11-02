022e7c48c26e7b87d1a168bacf75bd75f1ed1d01
Drew <drew@morphdev.co>
2022-11-02 13:31:39 -0500

    Complete refactor of code

--- src/App.tsx
+++ src/App.tsx
@@ -12,142 +12,191 @@ interface MenuItems {
   value: string
 }
 
+export type TFile = {
+  source?: string;
+  file?: File;
+}
+
+interface FormIoResponse {
+  data: {
+    components: [
+      {
+        data: {
+          values: [{
+            label: string,
+            value: string
+          }]
+        },
+        input: boolean,
+        key: string,
+        label: string,
+        tableView: boolean,
+        type: string
+      }
+    ]
+    _id: string
+  }
+}
+
+interface UserResponse {
+  index: number,
+  message: string,
+}
+
+interface dataPost {
+  data: {
+
+  }
+}
+
 function App() {
-  const [stepOne, setStepOne] = useState(false);
-  const [firstQuestion, setFirstQuestion] = useState(true);
-  const [secondQuestion, setSecondQuestion] = useState(false);
-  const [thirdQuestion, setThirdQuestion] = useState(false);
-  const [test, setTest] = useState('');
-  const [secondTest, setSecondTest] = useState('');
-  const [item, setItem] = useState('');
-  const [timesPerMonth, setTimesPerMonth] = useState('');
-  const [formIoData, setFormIoData] = useState<any>()
-  const [list, setList] = useState<MenuItems[]>([]);
-  const [days, setDays] = useState<MenuItems[]>([]);
+  const [formIoData, setFormIoData] = useState<FormIoResponse>()
+  const [image, setImage] = useState<TFile[]>([]);
+  const [sessionStarted, setSessionStarted] = useState(false);
+  const [submit, setSubmit] = useState(false);
+  const [index, setIndex] = useState(0)
 
-  useEffect(() => {
-    addResponseMessage('Hello, could we ask you some questions about Chipotle? We will store your display name and any message or content you share with us, and we will use this only for research purposes. This study is being conducted by Sympler, a research company. To start suervey, go ahead and click that "OK" button! By clicking "OK", you consent to the collection and use of any information you provide.');
-  }, []);
+  const [formSubmissionId, setFormSubmissionId] = useState('')
 
-  const handleNewUserMessage = (newMessage: string) => {
-    console.log(`New message incoming! ${newMessage}`);
-    // Now send the message throught the backend API
-    if (newMessage === 'OK' && firstQuestion) {
-      setQuickButtons([])
-      toggleMsgLoader();
-      setTimeout(() => {
-        addResponseMessage(formIoData.data.components[0].label)
-        setQuickButtons(list)
-        setFirstQuestion(false)
-        setSecondQuestion(true)
-        toggleMsgLoader();
-      },1500)
-    } else if (newMessage === 'NO THANKS' && firstQuestion) {
-      setFirstQuestion(false)
-      
-      return
-    }
-    if (newMessage && secondQuestion) {
-      setItem(newMessage);
-      toggleMsgLoader();
-      setQuickButtons([])
-      setTimeout(() => {
-        addResponseMessage(formIoData.data.components[1].label)
-        setQuickButtons(days)
-        toggleMsgLoader();
-        setSecondQuestion(false)
-        setThirdQuestion(true)
-      },1500)
-    }
-    if (newMessage && thirdQuestion) {
-      console.log('is this working')
-      setTimesPerMonth(newMessage);
-      setQuickButtons([])
-      toggleMsgLoader();
-      setTimeout(() => {
-        addResponseMessage('Thanks for chatting! Your input is valuable to us!')
-        toggleMsgLoader();
-      },1500)
-    }
-  };
+  const newDate = new Date().toString();
 
   useEffect(() => {
     // {{projectUrl}}/form/{{formId}}
-    axios.get(`https://ajszgfebbsbyxvg.form.io/chipotlechat`).then(res =>{
-      console.log('get result', res)
+    axios.get(`https://mykkomypewprmxq.form.io/funform`).then(res => {
+      console.log('get results from formIo', res)
       setFormIoData(res)
-      setList(res.data.components[0].data.values.map((e: any) => {
-        return e
-      }))
-      setDays(res.data.components[1].data.values.map((e: any) => {
-        return e
-      }))
     }).catch(error => {
       console.log('get error', error)
     })
-  },[])
+  }, [])
 
-  console.log('form',formIoData)
+  console.log('submtting?', submit)
+  console.log('index', index)
+
+  const submitData = async (message: string, index: number) => {
+    if (formIoData) {
+      if (formIoData.data._id && sessionStarted === false) {
+        axios.get(`https://mykkomypewprmxq.form.io/funform/submission/${formIoData.data._id}`).then(res => {
+          console.log('the res from formIo submission', res)
+        }).catch(async error => {
+          console.log('get submission error error', error)
+          const key = formIoData.data.components[index].key
+          const obj: any = {};
+          obj[key] = message
+          console.log('first obj', obj)
+          await axios.post(`https://mykkomypewprmxq.form.io/funform/submission`, {
+            data: {
+              ...obj
+            }
+          }).then(result => {
+            console.log('post create submission result', result)
+            setFormSubmissionId(result.data._id)
+            setSessionStarted(true)
+            setIndex(index + 1)
+            console.log('this is running lmao')
+            setSubmit(true)
+          }).catch(error => {
+            console.log('error', error)
+          })
+        })
+      } else if (formIoData.data._id && sessionStarted) {
+        const key = formIoData.data.components[index].key
+        const obj: any = {};
+        obj[key] = message
+        console.log('obj on put', obj)
+        console.log('submission id', formIoData.data._id)
+        axios.put(`https://mykkomypewprmxq.form.io/funform/submission/${formSubmissionId}`, {
+          data: {
+            ...obj
+          }
+        }).then(result => {
+          console.log('result from put', result)
+          setIndex(index + 1)
+        }).catch(error => {
+          console.log('error', error)
+        })
+      }
+    }
+  }
+
+  const askQuestion = async (message?: string) => {
+    console.log('askquesion is being run')
+    let i = index
+    if (formIoData) {
+      console.log('askquesion formdata')
+      if (index >= formIoData?.data.components.length - 1) {
+        console.log('all questions have been answered')
+      } else if (formIoData.data.components[index].label.includes('GetTimeZone')) {
+        console.log('askquesion is being run, inside else if')
+        // setSubmit(true)
+        await submitData(newDate.slice(newDate.indexOf('('), newDate.lastIndexOf(')') + 1), index)
+        return
+      }
+      if (message) {
+        await submitData(message, index)
+          addResponseMessage(formIoData.data.components[index].label)
+        if (formIoData.data.components[index].data) {
+          console.log('hello why is this not working')
+          setQuickButtons(formIoData.data.components[index].data.values ?? [])
+        } else {
+          setQuickButtons([])
+        }
+      } else {
+        console.log('index before it adds reponse', index)
+        addResponseMessage(formIoData.data.components[index].label)
+        if (formIoData.data.components[index].data) {
+          console.log('hello why is this not working')
+          setQuickButtons(formIoData.data.components[index].data.values ?? [])
+        } else {
+          setQuickButtons([])
+        }
+        if (message) {
+          console.log('askquesion is being run not insided')
+          console.log('new message', message)
+          await submitData(message, index)
+        }
+      }
+    }
+  }
 
   useEffect(() => {
-    if (timesPerMonth !== '') {
-      submitForm(); 
-    }
-  },[timesPerMonth])
+    console.log('is this running')
+    askQuestion()
+  }, [index, formIoData])
+
+  const sendImageFile = (p: TFile[]) => {
+    console.log('image has been updated', p)
+    setImage(p)
+  }
+
+  console.log('form', formIoData)
 
   const hanleQuckButtonClick = (e: string) => {
     addUserMessage(e);
-    handleNewUserMessage(e);
+    askQuestion(e);
   };
 
-  let buttons = [
-    {
-      label: 'OK',
-      value: 'OK'
-    },
-    {
-      label: 'NO THANKS',
-      value: 'N0 THANKS',
-    },
-  ];
-
   useEffect(() => {
-    setQuickButtons(buttons)
+    // setQuickButtons(buttons)
     toggleWidget()
-  },[]);
+  }, []);
 
-  const submitForm = async () => {
-    console.log(item)
-    await axios.post(`https://ajszgfebbsbyxvg.form.io/chipotlechat/submission`, {
-      data: {
-        whatItemDoYouGet9TimesOutOf10WhenYouEatAtChipotle: item, 
-        howManyDaysAMonthDoYouEatAtChipotle: timesPerMonth
-      }
-    }).then(result =>  {
-      console.log('result', result)
-    }).catch(error => {
-      console.log('error', error)
-    })
-  }
+  console.log('file?', image)
 
-  const sendImageFile = (p: string) => {
-    console.log('image has been updated')
-  }
-
-    return (
-      <div className="App">
-        <Widget
-          handleNewUserMessage={handleNewUserMessage}
-          profileAvatar={logo}
-          title="Messages"
-          subtitle="Chipotle Questions"
-          handleQuickButtonClicked={hanleQuckButtonClick}
-          emojis={false}
-          imagePreview={true}
-          sendImageFile={sendImageFile}
-        />
-      </div>
-    );
+  return (
+    <div className="App">
+      <Widget
+        handleNewUserMessage={askQuestion}
+        profileAvatar={logo}
+        title="Messages"
+        subtitle="Chipotle Questions"
+        handleQuickButtonClicked={hanleQuckButtonClick}
+        emojis={false}
+        imagePreview={true}
+        sendImageFile={sendImageFile}
+      />
+    </div>
+  );
 }
 
 export default App;
