93788b2f0eae533a58ff0e5ea1d51eec5d25548d
Drew <drew@morphdev.co>
2022-11-11 14:24:26 -0600

    Fixed a build issue and made the standalone

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
-
+    ReactDOM.render(
+      <App endpoint='mykkomypewprmxq' formName='funform' />,
+      document.getElementById('root')
+    );
